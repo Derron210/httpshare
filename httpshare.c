3b17e8ecf1995a9eb3612122334921143b07117e
Derron210 <derronishe@yandex.ru>
2025-03-02 21:07:23 +0300

    First commit

---
+++ httpshare.c
@@ -0,0 +1,129 @@
+
+#include "stdio.h"
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024*1024
+
+char buf[BUF_SIZE];
+
+int getFileSize(char* file) {
+    FILE* fp = fopen(file, "rb");
+    if (fp == NULL) {
+        return 0;
+    }
+    fseek(fp, 0L, SEEK_END);
+    int size = ftell(fp);
+    fclose(fp);
+    return size;
+}
+
+char* getFileName(char *path)
+{
+    int len = strlen(path);
+    int flag=0;
+     
+    for(int i=len-1; i>0; i--)
+    {
+        if(path[i]=='\\' || path[i] == '//' || path[i]=='/' )
+        {
+            flag=1;
+            path = path+i+1;
+            break;
+        }
+    }
+    return path;
+}
+
+int createServerSocket(int port) {
+    int server_fd;
+    ssize_t valread;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr*)&address,
+             sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    return server_fd;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Shoud use with <port> <file>\r\n");
+        return 1;
+    }
+
+    short port = strtol(argv[1], NULL, 10);
+    char* filePath = argv[2];
+
+    int fileSize = getFileSize(filePath);
+    if (fileSize == 0) {
+        printf("Couldn't get file\n");
+        return 1;
+    }
+
+    int serverSocket = createServerSocket(port);
+    printf("Waiting for connection\r\n");
+    int clientSocket = accept(serverSocket, NULL, NULL);
+
+    if (clientSocket < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    int requestBytes = read(clientSocket, buf, BUF_SIZE);
+    if (requestBytes <= 0) {
+        printf("read request error\r\n");
+        goto closeSockets;
+    }
+
+    sprintf(buf, "HTTP/1.1 200 OK\n"
+       "Content-Disposition: attachment; filename=\"%s\"\n"
+       "Content-Type: application/octet-stream\n"
+       "Keep-Alive: timeout=2, max=1"
+       "Connection: Keep-Alive"
+       "Content-Length: %d\n\n\0", getFileName(filePath), fileSize);
+    send(clientSocket, buf, strlen(buf), 0);
+
+    FILE* file = fopen(filePath, "rb");
+    int fd = fileno(file);
+    int i = 0;
+
+    while (i < fileSize) {
+        int actualRead = read(fd, buf, BUF_SIZE);
+        i += actualRead;
+        send(clientSocket, buf, actualRead, 0);
+    }
+    fclose(file);
+    printf("File sent\r\n");
+
+closeSockets:
+    int optReuse = 1;
+    setsockopt(serverSocket,SOL_SOCKET,SO_REUSEADDR, &optReuse, sizeof(int));
+    close(clientSocket);
+    close(serverSocket);
+
+    return 0;
+}
